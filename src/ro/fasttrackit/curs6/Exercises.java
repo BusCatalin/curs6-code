79a16d379611610a875831f3364037115012a801
Bus Catalin <buscatalindanut@yahoo.com>
2021-10-18 21:17:19 +0300

    Initial commit

---
+++ src/ro/fasttrackit/curs6/Exercises.java
@@ -0,0 +1,15 @@
+package ro.fasttrackit.curs6;
+
+public class Exercises {
+    public static void main(String[] args){
+        String[] names = {"Ana", "Dana", "Daria",};
+        printLongerThan(names, 4);
+    }
+    static void printLongerThan(String[] names,int minLenght){
+        for(String name:names){
+            if(name.length() >= minLenght){
+                System.out.println(name);
+            }
+        }
+    }
+}
